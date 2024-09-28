6ad8130f8d46f94425b434d727d6fc4d55473bd0
Stefano Trejo <stides11@gmail.com>
2024-09-27 17:15:16 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import Landingpage from './components/Landingpage';
+
+function App() {
+
+  return (
+    <div className='container'>
+
+      {/* Add the stars container */}
+      <div className="stars">
+
+        {/* Stars should be inside the .stars container */}
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+      </div>
+
+      {/* Nebula elements */}
+      <div className="nebula"></div>
+      <div className="nebula"></div>
+      <div className="nebula"></div>
+      <div className="nebula"></div>
+      <div className="nebula"></div>
+      <div className="nebula"></div>
+      
+      {/* Your landing page component */}
+      
+    </div>
+  )
+}
+
+export default App;
