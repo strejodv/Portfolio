a788d651c001d0723aa5c5b3d66d3d1f7beb118a
Stefano Trejo <stides11@gmail.com>
2024-09-29 00:19:51 -0700

    more functionality for carrousel component

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -8,30 +8,35 @@ import image3 from '../components/assets/img3.png';
 const Landingpage = () => {
   return (
     <div className="carousel-container">
+      <input type="radio" name="slider" id="radio1" defaultChecked />
+      <input type="radio" name="slider" id="radio2" />
+      <input type="radio" name="slider" id="radio3" />
+
       <div className="carousel">
-        <div className="slide">
+        <div className="slide slide1">
           <img src={image1} alt="Image 1" />
           <h1>Weather App</h1>
-          <p>Weather app the works by fetching real-time data from OpenWeatherMap API</p>
+          <p>Weather app that works by fetching real-time data from OpenWeatherMap API.</p>
         </div>
-        <div className="slide">
+        <div className="slide slide2">
           <img src={image2} alt="Image 2" />
-          <h2>Image 2 Header</h2>
-          <p>This is a description for Image 2.</p>
+          <h1>AI Chatbot Web Application</h1>
+          <p>Interactive chatbot using React and the ChatGPT API that mimics real conversations.</p>
         </div>
-        <div className="slide">
+        <div className="slide slide3">
           <img src={image3} alt="Image 3" />
-          <h2>Image 3 Header</h2>
-          <p>This is a description for Image 3.</p>
+          <h1>Quote Generator Web Application</h1>
+          <p>React app that pulls in quotes from a public API.</p>
         </div>
-    </div>
-      <div className="carousel-nav">
-        <label htmlFor="slide1"></label>
-        <label htmlFor="slide2"></label>
-        <label htmlFor="slide3"></label>
       </div>
-  </div>
+
+      <div className="carousel-nav">
+        <label htmlFor="radio1"></label>
+        <label htmlFor="radio2"></label>
+        <label htmlFor="radio3"></label>
+      </div>
+    </div>
   );
-}
+};
 
 export default Landingpage;
