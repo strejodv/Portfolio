cd254c185f2403d932c1902da701d033cd10da48
Stefano Trejo <stides11@gmail.com>
2024-10-01 21:36:01 -0700

    fixed 90% of responsive bug

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -3,10 +3,10 @@ import './Herosection.css';
 
 const Herosection = () => {
   return (
-    <section class="hero-wrapper">
-        <div class="hero-content">
-            <h1 class="hero-title">Stefano Trejo</h1>
-            <p class="hero-description">Hello, World! I know, it’s so cliché, right? Anyway, I'm a Front-End Software Engineer specializing in building responsive and scalable web applications. I work with technologies like ReactJS, JavaScript, Node.js, HTML, CSS, and Tailwind, among others.</p>
+    <section className="hero-wrapper">
+        <div className="hero-content">
+            <h1 className="hero-title">Stefano Trejo</h1>
+            <p className="hero-description">Hello, World! I know, it’s so cliché, right? Anyway, I'm a Front-End Software Engineer specializing in building responsive and scalable web applications. I work with technologies like ReactJS, JavaScript, Node.js, HTML, CSS, and Tailwind, among others.</p>
         </div>
     </section>
   )
